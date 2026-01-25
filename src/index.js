36358a4fae91ba805bfefabf4a90e88227bce5d3
danielyu128 <danielyu128@gmail.com>
2026-01-25 17:50:10 -0500

    Add styling for header

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import loadHeader from "./header.js";
 console.log(greeting);
 
 const content = document.querySelector("#content");
+const {header,homeButton,menuButton,aboutButton} = loadHeader();
+
+document.body.prepend(header);
+
