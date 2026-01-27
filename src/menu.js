245c78cbb741adbe0418fc5eb1611a6511288da1
danielyu128 <danielyu128@gmail.com>
2026-01-26 21:34:01 -0500

    Add elements to DOM

--- src/menu.js
+++ src/menu.js
@@ -58,6 +58,8 @@ export default function loadMenu() {
     smallPlatesItem1Price.classList.add("smallPlatesItem1Price");
     smallPlatesItem1Price.textContent = "$14";
 
+    smallPlatesItem1NamePrice.append(smallPlatesItem1Name, smallPlatesItem1Price);
+
 
     //Div that contains the bio
     const smallPlatesItem1Info = document.createElement("div");
@@ -69,6 +71,9 @@ export default function loadMenu() {
     const smallPlatesItem1Dietary = document.createElement("div");
     smallPlatesItem1Dietary.classList.add("smallPlatesItem1Dietary");
     smallPlatesItem1Dietary.textContent = "Vegetarian, Gluten Free";
+
+    smallPlatesItem1.append(smallPlatesItem1NamePrice, smallPlatesItem1Info, smallPlatesItem1Dietary);
+
 
     //Item 2
     const smallPlatesItem2 = document.createElement("div");
@@ -87,6 +92,10 @@ export default function loadMenu() {
     smallPlatesItem2Price.classList.add("smallPlatesItem2Price");
     smallPlatesItem2Price.textContent = "$17";
 
+    smallPlatesItem2NamePrice.append(smallPlatesItem2Name, smallPlatesItem2Price);
+
+
+
     //Div that contains the bio
     const smallPlatesItem2Info = document.createElement("div");
     smallPlatesItem2Info.classList.add("smallPlatesItem2Info");
@@ -96,6 +105,10 @@ export default function loadMenu() {
     const smallPlatesItem2Dietary = document.createElement("div");
     smallPlatesItem2Dietary.classList.add("smallPlatesItem2Dietary");
     smallPlatesItem2Dietary.textContent = "Spicy";
+
+    smallPlatesItem2.append(smallPlatesItem2NamePrice, smallPlatesItem2Info, smallPlatesItem2Dietary);
+
+
 
 
     //Item 3
@@ -115,6 +128,9 @@ export default function loadMenu() {
     smallPlatesItem3Price.classList.add("smallPlatesItem3Price");
     smallPlatesItem3Price.textContent = "$18";
 
+    smallPlatesItem3NamePrice.append(smallPlatesItem3Name, smallPlatesItem3Price);
+
+
 
     //Div that contains the bio
     const smallPlatesItem3Info = document.createElement("div");
@@ -126,6 +142,9 @@ export default function loadMenu() {
     const smallPlatesItem3Dietary = document.createElement("div");
     smallPlatesItem3Dietary.classList.add("smallPlatesItem3Dietary");
     smallPlatesItem3Dietary.textContent = "$18";
+
+    smallPlatesItem3.append(smallPlatesItem3NamePrice, smallPlatesItem3Info, smallPlatesItem3Dietary);
+
 
 
     //Item 4
@@ -145,6 +164,9 @@ export default function loadMenu() {
     smallPlatesItem4Price.classList.add("smallPlatesItem4Price");
     smallPlatesItem4Price.textContent = "$19";
 
+    smallPlatesItem4NamePrice.append(smallPlatesItem4Name, smallPlatesItem4Price);
+
+
 
     //Div that contains the bio
     const smallPlatesItem4Info = document.createElement("div");
@@ -156,6 +178,17 @@ export default function loadMenu() {
     const smallPlatesItem4Dietary = document.createElement("div");
     smallPlatesItem4Dietary.classList.add("smallPlatesItem4Dietary");
     smallPlatesItem4Dietary.textContent = "";
+
+    smallPlatesItem4.append(smallPlatesItem4NamePrice, smallPlatesItem4Info, smallPlatesItem4Dietary);
+
+
+    //Adding the four small dishes to the dish div
+    smallPlatesItems.append(smallPlatesItem1, smallPlatesItem2, smallPlatesItem3, smallPlatesItem4);
+
+    //Adding the title plus the dish div to the small plates template
+    smallPlates.append(smallPlatesTitle, smallPlatesItems);
+
+
 
 
 
@@ -191,6 +224,8 @@ export default function loadMenu() {
     mainCoursesItem1Price.classList.add("mainCoursesItem1Price");
     mainCoursesItem1Price.textContent = "$38";
 
+    mainCoursesItem1NamePrice.append(mainCoursesItem1Name, mainCoursesItem1Price);
+
 
     //Div that contains the bio
     const mainCoursesItem1Info = document.createElement("div");
@@ -202,6 +237,8 @@ export default function loadMenu() {
     const mainCoursesItem1Dietary = document.createElement("div");
     mainCoursesItem1Dietary.classList.add("mainCoursesItem1Dietary");
     mainCoursesItem1Dietary.textContent = "Gluten Free";
+
+    mainCoursesItem1.append(mainCoursesItem1NamePrice, mainCoursesItem1Info, mainCoursesItem1Dietary);
 
 
 
@@ -222,6 +259,8 @@ export default function loadMenu() {
     mainCoursesItem2Price.classList.add("mainCoursesItem2Price");
     mainCoursesItem2Price.textContent = "$34";
 
+    mainCoursesItem2NamePrice.append(mainCoursesItem2Name, mainCoursesItem2Price);
+
 
     //Div that contains the bio
     const mainCoursesItem2Info = document.createElement("div");
@@ -233,6 +272,9 @@ export default function loadMenu() {
     const mainCoursesItem2Dietary = document.createElement("div");
     mainCoursesItem2Dietary.classList.add("mainCoursesItem2Dietary");
     mainCoursesItem2Dietary.textContent = "Gluten Free";
+
+    mainCoursesItem2.append(mainCoursesItem2NamePrice, mainCoursesItem2Info, mainCoursesItem2Dietary);
+
 
 
     //Item 3
@@ -252,6 +294,9 @@ export default function loadMenu() {
     mainCoursesItem3Price.classList.add("mainCoursesItem3Price");
     mainCoursesItem3Price.textContent = "$29";
 
+    mainCoursesItem3NamePrice.append(mainCoursesItem3Name, mainCoursesItem3Price);
+
+
 
     //Div that contains the bio
     const mainCoursesItem3Info = document.createElement("div");
@@ -264,6 +309,7 @@ export default function loadMenu() {
     mainCoursesItem3Dietary.classList.add("mainCoursesItem3Dietary");
     mainCoursesItem3Dietary.textContent = "Vegetarian";
 
+    mainCoursesItem3.append(mainCoursesItem3NamePrice, mainCoursesItem3Info, mainCoursesItem3Dietary);
 
 
 
@@ -284,6 +330,9 @@ export default function loadMenu() {
     mainCoursesItem4Price.classList.add("mainCoursesItem4Price");
     mainCoursesItem4Price.textContent = "$42";
 
+    mainCoursesItem4NamePrice.append(mainCoursesItem4Name, mainCoursesItem4Price);
+
+
     //Div that contains the bio
     const mainCoursesItem4Info = document.createElement("div");
     mainCoursesItem4Info.classList.add("mainCoursesItem4Info");
@@ -296,7 +345,13 @@ export default function loadMenu() {
     mainCoursesItem4Dietary.textContent = "Spicy, Gluten Free";
 
 
+    mainCoursesItem4.append(mainCoursesItem4NamePrice, mainCoursesItem4Info, mainCoursesItem4Dietary);
 
+    //Adding the four main course dishes to the dish div
+    mainCoursesItems.append(mainCoursesItem1, mainCoursesItem2, mainCoursesItem3, mainCoursesItem4);
+
+    //Adding the title plus the dish div to the main course plates template
+    mainCourses.append(mainCoursesTitle, mainCoursesItems);
 
 
 
@@ -331,10 +386,14 @@ export default function loadMenu() {
     refreshmentsItem1Price.classList.add("refreshmentsItem1Price");
     refreshmentsItem1Price.textContent = "$13";
 
+    refreshmentsItem1NamePrice.append(refreshmentsItem1Name, refreshmentsItem1Price);
+
 
     const refreshmentsItem1Info = document.createElement("div");
     refreshmentsItem1Info.classList.add("refreshmentsItem1Info");
     refreshmentsItem1Info.textContent = "A refreshing blend of blood orange, prosecco, and a splash of soda.";
+
+    refreshmentsItem1.append(refreshmentsItem1Img, refreshmentsItem1NamePrice, refreshmentsItem1Info);
 
 
     //Refreshment 2
@@ -356,10 +415,15 @@ export default function loadMenu() {
     refreshmentsItem2Price.classList.add("refreshmentsItem2Price");
     refreshmentsItem2Price.textContent = "$9";
 
+    refreshmentsItem2NamePrice.append(refreshmentsItem2Name, refreshmentsItem2Price);
+
 
     const refreshmentsItem2Info = document.createElement("div");
     refreshmentsItem2Info.classList.add("refreshmentsItem2Info");
     refreshmentsItem2Info.textContent = "Cucumber, fresh mint, lime, and house-made tonic water.";
+
+    refreshmentsItem2.append(refreshmentsItem2Img, refreshmentsItem2NamePrice, refreshmentsItem2Info);
+
 
 
     //Refreshment 3
@@ -381,11 +445,31 @@ export default function loadMenu() {
     refreshmentsItem3Price.classList.add("refreshmentsItem3Price");
     refreshmentsItem3Price.textContent = "$8";
 
+    refreshmentsItem3NamePrice.append(refreshmentsItem3Name, refreshmentsItem3Price);
+
 
     const refreshmentsItem3Info = document.createElement("div");
     refreshmentsItem3Info.classList.add("refreshmentsItem3Info");
     refreshmentsItem3Info.textContent = "Cold-brewed black tea infused with raspberry and hibiscus.";
-    
+
+    refreshmentsItem3.append(refreshmentsItem3Img, refreshmentsItem3NamePrice, refreshmentsItem3Info);
+
+
+    //Adding each refreshment to the refreshment list
+    refreshmentsList.append(refreshmentsItem1, refreshmentsItem2, refreshmentsItem3);
+
+    //Adding the title and the refreshments list to the refreshment container
+    refreshments.append(refreshmentsTitle, refreshmentsList);
+
+
+    //Adding the small plates, main dishes, and refreshments to the main menu page
+
+    menuDishes.append(smallPlates, mainCourses, refreshments);
+
+    //Add to page
+    menuPage.append(menuDishes);
+
+
 
     return { menuPage };
 }
